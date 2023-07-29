aa0406367f91e421efa443d06b4c1e303cf3db98
Julian <Julian-Tekstra@hotmail.com>
2023-07-29 12:46:50 +0200

    Add more content

--- Assets/Scripts/Player/PlayerInputHandler.cs
+++ Assets/Scripts/Player/PlayerInputHandler.cs
@@ -10,7 +10,6 @@ public class PlayerInputHandler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(Query)
         controls = new PlayerControls();
         controls.Enable();
 
